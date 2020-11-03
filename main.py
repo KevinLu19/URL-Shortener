df78820ab4a74e0f9dca32dc1902e1d1286692a3
Kevin <Kevinxz01@gmail.com>
2020-11-03 08:54:50 -0500

    Very basic UI currently

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import PySide2
+
+from PySide2.QtWidgets import QApplication, QPushButton, QVBoxLayout, QDialog, QLineEdit
+from PySide2.QtQuick import QQuickView
+from PySide2.QtCore import Slot
+
+class URL_Class(QDialog):
+    def __init__(self, parent=None):
+        super(URL_Class, self).__init__(parent)
+
+        self.setWindowTitle("URL Shortener")
+
+        self.edit = QLineEdit("Enter URL")
+        self.button = QPushButton("Shortening URL")
+
+        layout_container = QVBoxLayout()
+        layout_container.addWidget(self.edit)
+        layout_container.addWidget(self.button)
+
+        self.setLayout(layout_container)
+
+        self.button.clicked.connect(self.short_url)
+
+    def short_url(self):
+        print ("Button connected")
+        return True
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    view = QQuickView()
+    url = URL_Class()
+    view.setResizeMode(QQuickView.SizeRootObjectToView)
+    url.show()
+
+    sys.exit(app.exec_())
