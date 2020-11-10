2a2fc614fb77875cb683d99d53392d626ab25129
Kevin <Kevinxz01@gmail.com>
2020-11-10 16:59:34 -0500

    Fixed genearted py file and cleaned up code

--- main.py
+++ main.py
@@ -1,58 +1,24 @@
+from url_gui_file import Ui_MainWindow
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 import sys
-import PySide2
 
-from PySide2.QtWidgets import QApplication, QPushButton, QVBoxLayout, QDialog, QLineEdit
-from PySide2.QtQuick import QQuickView
-from PySide2.QtCore import Slot, QFile, QIODevice
-from PySide2.QtUiTools import QUiLoader
+# url_gui_file.py generated via command
+# pyuic5 -x <.ui file> -o <output_file.py>
 
-class URL_Class(QDialog):
-    def __init__(self, parent=None):
-        super(URL_Class, self).__init__(parent)
+class MainFileClass(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainFileClass, self).__init__()
+        self.setupUi(self)
+        self.short_url_button.clicked.connect(self.temporary_button)
 
-        self.setWindowTitle("URL Shortener")
-
-        self.edit = QLineEdit("Enter URL")
-        self.button = QPushButton("Shortening URL")
-
-        layout_container = QVBoxLayout()
-        layout_container.addWidget(self.edit)
-        layout_container.addWidget(self.button)
-
-        self.setLayout(layout_container)
-
-        self.button.clicked.connect(self.short_url)
-
-    def short_url(self):
-        print ("Button connected")
-        return True
+    def temporary_button(self):
+        print ("Temproary button function connection.")
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-
-    # view = QQuickView()
-    # url = URL_Class()
-    # view.setResizeMode(QQuickView.SizeRootObjectToView)
-    # url.resize(800, 600)
-
-    ui_file = "url_gui.ui"
-
-    ui = QFile(ui_file)
-
-    if not ui.open(QIODevice.ReadOnly):
-        print (f"Cannot open {ui}: {ui.errorString()}")
-        sys.exit(1)
-
-    ui_loader = QUiLoader()
-    window_loader = ui_loader.load(ui)
-    ui.close()
-
-    if not window_loader:
-        print (ui_loader.errorString())
-        sys.exit(1)
-
-    window_loader.show()
-
-    #url.show()
-
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = MainFileClass()
+    ui.show()
     sys.exit(app.exec_())
