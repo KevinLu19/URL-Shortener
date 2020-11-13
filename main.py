ade06a8537e09934e6fc904756406e0e52bf298c
Kevin <Kevinxz01@gmail.com>
2020-11-13 18:02:56 -0500

    Connected two classes from different files

--- main.py
+++ main.py
@@ -12,16 +12,21 @@ class MainFileClass(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainFileClass, self).__init__()
         self.setupUi(self)
-        self.short_url_button.clicked.connect(self.temporary_button)
+        self.short_url_button.clicked.connect(self.send_user_url)
+        self.setWindowTitle("Short That URL")
+        self.__url_db = URL_DB_Class()
 
-    def temporary_button(self):
-        print ("Temproary button function connection.")
+    def send_user_url (self):
+        qt_url_text_edit = self.url_input_text.toPlainText()
+        self.__url_db.set_original_user_url(qt_url_text_edit)
+
+    # def temporary_button(self):
+    #     print ("Temproary button function connection.")
+    #     print ("stuff that's stores textedit: %s" % self.url_input_text.toPlainText())
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = MainFileClass()
-    db_class = URL_DB_Class()
-
     ui.show()
     sys.exit(app.exec_())
